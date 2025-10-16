d3fe010ef43836ee7551a739dcdc39f0f0652cc0
rayssasimoes <rayssasimoes27@gmail.com>
2025-10-16 08:34:03 -0300

    feat(homepage): Exibe lista de jogos do banco de dados.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GameController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
